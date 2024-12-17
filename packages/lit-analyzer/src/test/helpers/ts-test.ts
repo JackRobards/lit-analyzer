a5b8255ef8e53d4c4fa4d4c8f1ada7c7ae2ac18e
Jack Robards <jrobards2@gmail.com>
2024-12-17 00:12:34 -0600

    chore: Fix TypeScript build to match the new version of ava

--- packages/lit-analyzer/src/test/helpers/ts-test.ts
+++ packages/lit-analyzer/src/test/helpers/ts-test.ts
@@ -1,9 +1,10 @@
-import test, { Implementation } from "ava";
+import test, { ImplementationFn } from "ava";
 import { dirname } from "path";
 import * as tsModule from "typescript";
 import { setTypescriptModule } from "../../lib/analyze/ts-module.js";
 
-type TestFunction = (title: string, implementation: Implementation) => void;
+// Based on the `ava` types for test.only and test.skip. This matches the format of the tests in this repo
+type TestFunction = (title: string, implementation: ImplementationFn<unknown[]>) => void;
 
 const TS_MODULES_ALL = ["current", "4.8", "5.0", "5.1"] as const;
 
@@ -86,13 +87,13 @@ export function getCurrentTsModuleDirectory(): string {
  * @param title
  * @param cb
  */
-function setupTest(testFunction: TestFunction, tsModuleKind: TsModuleKind | undefined, title: string, cb: Implementation) {
+function setupTest(testFunction: TestFunction, tsModuleKind: TsModuleKind | undefined, title: string, cb: ImplementationFn<unknown[]>) {
 	// Generate title based on the ts module
 	const version = getTsModuleWithKind(tsModuleKind).version;
 	const titleWithModule = `[ts${version}] ${title}`;
 
 	// Setup up the ava test
-	testFunction(titleWithModule, (...args: Parameters<Implementation>) => {
+	testFunction(titleWithModule, (...args: Parameters<ImplementationFn<unknown[]>>) => {
 		// Set the ts module as environment variable before running the test
 		setCurrentTsModuleKind(tsModuleKind);
 
@@ -100,7 +101,9 @@ function setupTest(testFunction: TestFunction, tsModuleKind: TsModuleKind | unde
 		// Remove this line after refactoring away the global "tsModule.ts"
 		setTypescriptModule(getCurrentTsModule());
 
-		const res = cb(...args);
+		const [t, ...restArgs] = args;
+
+		const res = cb(t, ...restArgs);
 
 		// Reset the selected TS_MODULE
 		setCurrentTsModuleKind(undefined);
@@ -116,7 +119,7 @@ function setupTest(testFunction: TestFunction, tsModuleKind: TsModuleKind | unde
  * @param title
  * @param cb
  */
-function setupTests(testFunction: (title: string, implementation: Implementation) => void, title: string, cb: Implementation) {
+function setupTests(testFunction: TestFunction, title: string, cb: ImplementationFn<unknown[]>) {
 	// Find the user specified TS_MODULE at setup time
 	const moduleKinds: TsModuleKind[] = (() => {
 		const currentTsModuleKind = getCurrentTsModuleKind();
